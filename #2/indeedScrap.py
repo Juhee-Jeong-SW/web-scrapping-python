32f230ec02823c9080f007c0380689d1f44db61f
Juhee Jeong <stellajuhee@gmail.com>
2020-12-19 22:04:03 +0900

    ~#2.7 Extracting Companies

--- #2/indeedScrap.py
+++ #2/indeedScrap.py
@@ -28,9 +28,29 @@ def extract_indeed_pages():
     return max_page
 
 def extract_indeed_jobs(last_page):
-    for page in range(last_page):
-        result = requests.get(f"{INDEED_URL}&start={page*LIMIT}")
-        print(result.status_code)
+    jobs = []
+
+    #for page in range(last_page):
+    result = requests.get(f"{INDEED_URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    #print(result.status_code)
+    results = soup.find_all("div", {"class" : "jobsearch-SerpJobCard"})
+    #print(results)
+    
+    for result in results:
+        #title = result.find("div", { "class" : "title" })
+        #anchor = title.find("a")["title"] # 난 여기서 오류남..
+        company = result.find("span", {"class" : "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None: 
+            company = str(company_anchor.string)
+        else:
+            company = str(company.string)
+        company =  company.strip() # 빈 칸 없애주기
+        print(company)
+
+
+    return jobs
 
 last_indeed_page = extract_indeed_pages()
 
